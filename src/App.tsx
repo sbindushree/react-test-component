eab290831d2fe085e90de7b45d3c73c61540465b
Bindushree S <bindushree@Bindushrees-MacBook-Air.local>
2023-01-12 01:12:24 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "@sbindushree/react-test-components-library";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button label="Hello world!"/>
       </header>
     </div>
   );
